fd402cd4c7fbc1f1c018753decda30a57a0b0868
maticn <maticnova@gmail.com>
2017-10-10 15:37:48 +0200

    Started implementing tests.

---
+++ JHotDraw/src/test/java/org/jhotdraw/draw/action/EdgeDetectionTest.java
@@ -0,0 +1,26 @@
+package org.jhotdraw.draw.action;
+
+/**
+ *
+ * @author Matic-ProBook
+ */
+
+import org.junit.Test;
+
+public class EdgeDetectionTest {
+
+    @Test
+    public void edgeDetectionTest() {
+//    Money m12CHF= new Money(12, "CHF");
+//    Money m14CHF= new Money(14, "CHF");
+//    
+//    Money expected= new Money(26, "CHF");
+//    Money result= m12CHF.add(m14CHF);
+//    assertTrue(expected.equals(result));
+
+        EdgeDetectionAction eda = new EdgeDetectionAction(null);
+        //eda.edgeDetection(view, figures);
+        
+    }
+
+}
