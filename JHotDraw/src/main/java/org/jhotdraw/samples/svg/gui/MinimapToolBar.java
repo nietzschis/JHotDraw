649cc2f7e481d3956e2f31711fbf9c7bd215d3e3
Jakan16 <Jakan16@student.sdu.dk>
2017-11-16 12:12:00 +0100

    Added dummy panel

--- JHotDraw/src/main/java/org/jhotdraw/samples/svg/gui/MinimapToolBar.java
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/gui/MinimapToolBar.java
@@ -5,6 +5,12 @@
  */
 package org.jhotdraw.samples.svg.gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import org.jhotdraw.draw.DrawingView;
 import org.jhotdraw.util.ResourceBundleUtil;
 
 /**
@@ -12,11 +18,13 @@ import org.jhotdraw.util.ResourceBundleUtil;
  * @author Jakob Andersen
  */
 public class MinimapToolBar extends AbstractToolBar {
+    
+    DrawingView drawingView;
+    
     /** Creates new instance. */
     public MinimapToolBar() {
         ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.samples.svg.Labels");
         setName(labels.getString(getID() + ".toolbar"));
-        setDisclosureStateCount(2);
     }
     
     @Override
@@ -24,5 +32,41 @@ public class MinimapToolBar extends AbstractToolBar {
         return "minimap";
     }
     
+    @Override
+    protected JComponent createDisclosedComponent(int state) {
+        JPanel p = null;
+        
+        if(state == 1){
+            p = new JPanel(){
+                
+                //@Override
+                //public Dimension getPreferredSize() {
+                //    return new Dimension(80, 80);
+                //}
+                
+                public void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    g.setColor(Color.white);
+                    g.fillRect(0, 0, 800, 800);
+                    
+                    g.setColor(Color.black);
+                    g.drawString("Dummy panel",10,20);
+                }  
+            };
+            
+            p.setPreferredSize(new Dimension(80,80));
+            //p.invalidate();
+        }
+        
+        return p;
+    }
     
+    @Override
+    protected int getDefaultDisclosureState() {
+        return 1;
+    }
+
+    public void setDrawingView(DrawingView d) {
+        drawingView = d;
+    }
 }
