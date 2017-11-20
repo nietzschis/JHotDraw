eeb30e569570b895fca307e96f2f2f6859b1ba9a
Unknown <mikkelbjerg95@gmail.com>
2017-11-20 21:40:14 +0100

    Implemented recording functionality
    Recording should now work and comments have been written to the code that has been written. Whomever implements editing might need to add a way of getting the frames recorded.

--- JHotDraw/src/main/java/org/jhotdraw/samples/svg/gui/RecordingToolBar.java
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/gui/RecordingToolBar.java
@@ -4,6 +4,8 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.prefs.Preferences;
 import javax.swing.AbstractAction;
 import javax.swing.AbstractButton;
@@ -12,6 +14,8 @@ import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import org.jhotdraw.draw.DrawingEditor;
+import org.jhotdraw.draw.FigureEvent;
+import org.jhotdraw.draw.FigureListener;
 import org.jhotdraw.gui.plaf.palette.PaletteButtonUI;
 import org.jhotdraw.util.ResourceBundleUtil;
 
@@ -19,7 +23,6 @@ public class RecordingToolBar extends AbstractToolBar {
     public RecordingToolBar() {
         ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.samples.svg.Labels");
         setName(labels.getString("recording.toolbar"));
-        recordingManager = new RecordingManager(this.editor);
     }
     
     RecordingManager recordingManager = null;
@@ -81,18 +84,57 @@ public class RecordingToolBar extends AbstractToolBar {
         return 1;
     }
     
+    /**
+     * setEditor has been overridden so that the RecordingManager instance is constructed with a editor
+     * rather than just null. This is necessary because the toolbar doesn't get the editor when it's created by SVGDrawingPanel.
+     */
+    @Override
+    public void setEditor(DrawingEditor editor) {
+        super.setEditor(editor);
+        recordingManager = new RecordingManager(editor);
+    }
+    
     private class RecordingManager {
+        /** 
+         * Action that initiates the recording implemented by extending AbstractAction to not stray from what other toolbars do.
+         */
         public class StartRecordingAction extends AbstractAction {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Start");
+                startRecordingActiveDrawing();
             }
         }
         
+        /** 
+         * Action that stops the recording implemented by extending AbstractAction to not stray from what other toolbars do.
+         */
         public class StopRecordingAction extends AbstractAction {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Stop");
+                stopRecordingActiveDrawing();
+            }
+        }
+        
+        /**
+         * Class used to store data relevant for later replaying and editing.
+         * The drawing variable should be used for storing the Drawing received from the DrawingView,
+         * and the klass variable should be used for storing the class of the source that triggered the creation of a new frame instance.
+         */
+        public class Frame {
+            private final Object drawing;
+            private final Class klass;
+            
+            public Frame(Object drawing, Class klass) {
+                this.drawing = drawing;
+                this.klass = klass;
+            }
+            
+            public Object getDrawing() {
+                return drawing;
+            }
+            
+            public Class getKlass() {
+                return klass;
             }
         }
         
@@ -104,10 +146,22 @@ public class RecordingToolBar extends AbstractToolBar {
         private StartRecordingAction startRecordingAction = null;
         private StopRecordingAction stopRecordingAction = null;
         
+        // Used for storing Frame instances associated with a specific Drawing based on that Drawing's hashcode.
+        private HashMap<Integer, ArrayList<Frame>> mapOfDrawingFrames = new HashMap<>();
+        
+        // Used for storing FigureListeners created for the purpose of recording. Stored for the purpose of later removing them again.
+        private HashMap<Integer, FigureListener> mapOfFigureListeners = new HashMap<>();
+        
+        /**
+         * Can be used for setting the editor if it for some reason changes after the initial setup of the application.
+         */
         public void setEditor(DrawingEditor editor) {
             this.editor = editor;
         }
         
+        /**
+         * Lazy initializes the StartRecordingAction used to initiate the recording of a Drawing.
+         */
         public StartRecordingAction getStartRecordingAction() {
             if(startRecordingAction == null) {
                 startRecordingAction = new StartRecordingAction();
@@ -116,12 +170,103 @@ public class RecordingToolBar extends AbstractToolBar {
             return startRecordingAction;
         }
         
+        /**
+         * Lazy initializes the StopRecordingAction used to stop the recording of a Drawing.
+         */
         public StopRecordingAction getStopRecordingAction() {
             if(stopRecordingAction == null) {
                 stopRecordingAction = new StopRecordingAction();
             }
             
             return stopRecordingAction;
+        }
+        
+        /**
+         * Creates a FigureListener and attaches it to the active Drawing.
+         */
+        private void startRecordingActiveDrawing() {
+            if(editor == null) {
+                System.err.println("Cannot record when there is no active editor!");
+                return;
+            }
+            
+            int hashcode = editor.getActiveView().getDrawing().hashCode();
+            mapOfDrawingFrames.putIfAbsent(hashcode, new ArrayList<Frame>());
+            mapOfFigureListeners.putIfAbsent(hashcode, createFigureListener(hashcode));
+            editor.getActiveView().getDrawing().addFigureListener(mapOfFigureListeners.get(hashcode));
+        }
+        
+        /**
+         * Removes the FigureListener from the active Drawing as well as the map where in listeners are stored, 
+         * it does however not delete the frames that have been recorded though.
+         */
+        private void stopRecordingActiveDrawing() {
+            int hashcode = editor.getActiveView().getDrawing().hashCode();
+            editor.getActiveView().getDrawing().removeFigureListener(mapOfFigureListeners.get(hashcode));
+            mapOfFigureListeners.remove(hashcode);
+        }
+        
+        /**
+         * Not currently used, but could be used for clearing out frames that were previously recorded.
+         */
+        private void clearFramesOfActiveDrawing() {
+            int hashcode = editor.getActiveView().getDrawing().hashCode();
+            mapOfDrawingFrames.get(hashcode).clear();
+        }
+        
+        /**
+         * Creates a FigureListener associated with a specific Drawing and stores 
+         * clones of that same drawing based on a hash obtained from that Drawing's hashcode function.
+         */
+        private FigureListener createFigureListener(int hashcode) {
+            return new FigureListener() {
+                /**
+                 * Because we wanna store all changes this is cleaner than pasting the same code to all handlers.
+                 * What this does is it creates a new frame which is fed a clone of the active Drawing as well as the
+                 * class of the figure that triggered the event.
+                 */
+                private void storeChange(FigureEvent e) {
+                    mapOfDrawingFrames.get(hashcode).add(new Frame(
+                            editor.getActiveView().getDrawing().clone(),
+                            e.getSource().getClass()
+                    ));
+                }
+                
+                @Override
+                public void areaInvalidated(FigureEvent e) {
+                    storeChange(e);
+                }
+
+                @Override
+                public void attributeChanged(FigureEvent e) {
+                    storeChange(e);
+                }
+
+                @Override
+                public void figureHandlesChanged(FigureEvent e) {
+                    storeChange(e);
+                }
+
+                @Override
+                public void figureChanged(FigureEvent e) {
+                    storeChange(e);
+                }
+
+                @Override
+                public void figureAdded(FigureEvent e) {
+                    storeChange(e);
+                }
+
+                @Override
+                public void figureRemoved(FigureEvent e) {
+                    storeChange(e);
+                }
+
+                @Override
+                public void figureRequestRemove(FigureEvent e) {
+                    storeChange(e);
+                }
+            };
         }
     }
 }
