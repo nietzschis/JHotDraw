d4ca0aa5bff5f4446c6f619e407adf9fec31fe73
Strange96 <maha516@student.sdu.dk>
2017-11-21 13:56:17 +0100

    Triangle test

---
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGTriangleFigureTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jhotdraw.samples.svg.figures;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Collection;
+import org.jhotdraw.draw.ConnectionFigure;
+import org.jhotdraw.draw.Connector;
+import org.jhotdraw.draw.Handle;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mathias
+ */
+public class SVGTriangleFigureTest {
+    
+    public SVGTriangleFigureTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+    /**
+     * Test of class SVGTriangleFigure.
+     * Creating a triangle figure should create
+     * a figure in a bounding box
+     */
+    @Test
+    public void testTriangleFigure() {
+        SVGTriangleFigure instance = new SVGTriangleFigure();
+        Rectangle2D.Double expResult = 
+                new Rectangle2D.Double(
+                        instance.getX(), instance.getY(), 
+                        instance.getWidth(), instance.getHeight() );
+        Rectangle2D.Double result = instance.getBounds();
+        assertEquals(expResult, result);
+    }
+
+}
