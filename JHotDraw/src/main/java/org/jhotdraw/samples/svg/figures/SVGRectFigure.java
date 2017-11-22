ce8821e748ec08b2a69eeee5842a18874d7698f5
Mads Mosumgaard <mads1108@hotmail.dk>
2017-11-22 03:25:23 +0100

    fixed some merge errors

--- JHotDraw/src/main/java/org/jhotdraw/samples/svg/figures/SVGRectFigure.java
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/figures/SVGRectFigure.java
@@ -178,10 +178,14 @@ public class SVGRectFigure extends SVGAttributedFigure implements SVGFigure {
     @Override
     public void setBounds(Point2D.Double anchor, Point2D.Double lead) {
         invalidateTransformedShape();
+        double arcWidthRatio = roundrect.width > 0d ? roundrect.arcwidth / 2d / roundrect.width : 0d;
+        double arcHeighRatio = roundrect.height > 0d ? roundrect.archeight / 2d / roundrect.height : 0d;
         roundrect.x = Math.min(anchor.x, lead.x);
         roundrect.y = Math.min(anchor.y, lead.y);
         roundrect.width = Math.max(0.1, Math.abs(lead.x - anchor.x));
         roundrect.height = Math.max(0.1, Math.abs(lead.y - anchor.y));
+        
+        setArc(roundrect.width*arcWidthRatio,roundrect.height*arcHeighRatio);
         invalidate();
     }
 
