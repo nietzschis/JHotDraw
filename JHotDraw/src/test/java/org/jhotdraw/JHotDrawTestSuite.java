90a83ea10068c083f5cd8f88b1a17ef3514dc7c5
Jan Corfixen Sørensen <corfixen@gmail.com>
2017-12-06 11:53:09 +0100

    Test suite added.

---
+++ JHotDraw/src/test/java/org/jhotdraw/JHotDrawTestSuite.java
@@ -0,0 +1,37 @@
+package org.jhotdraw;
+
+import org.jhotdraw.app.action.TinyPngCompressActionJGivenTest;
+import org.jhotdraw.app.action.TinyPngCompressActionTest;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * TODO: Dear co-developers. Please add your test to this suite.
+ *
+ * @author corfixen
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TinyPngCompressActionJGivenTest.class, TinyPngCompressActionTest.class})
+public class JHotDrawTestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
