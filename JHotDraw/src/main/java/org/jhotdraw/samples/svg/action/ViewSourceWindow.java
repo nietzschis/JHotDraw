5ef36e13509fc312bbd1a0dc39462f1a1029cf14
Marc Kristensen <marcndkk@gmail.com>
2017-12-07 15:40:39 +0100

    Moved ViewSourceWindow to its own file

---
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/action/ViewSourceWindow.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jhotdraw.samples.svg.action;
+
+import java.awt.Frame;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import org.jhotdraw.samples.svg.SVGView;
+import org.jhotdraw.samples.svg.io.SVGOutputFormat;
+
+/**
+ * Window for viewing source - Refactored for easier testing
+ *
+ * @author Marc
+ */
+public class ViewSourceWindow {
+
+    private final static String DIALOG_CLIENT_PROPERTY = "view.viewSource.dialog";
+
+    private final SVGView p;
+    private final JDialog dialog;
+    private final JTextArea ta;
+    private String source;
+
+    public ViewSourceWindow(SVGView p) throws IOException {
+        this.p = p;
+
+        this.source = generateSource(p);
+
+        if (p.getClientProperty(DIALOG_CLIENT_PROPERTY) == null) {
+            this.ta = new JTextArea(source);
+            this.dialog = createNewDialog(p, ta);
+        } else {
+            this.dialog = (JDialog) p.getClientProperty(DIALOG_CLIENT_PROPERTY);
+            this.ta = (JTextArea) ((JScrollPane) dialog.getContentPane().getComponent(0)).getViewport().getView();
+            this.ta.setText(source);
+        }
+    }
+
+    private String generateSource(SVGView p) throws IOException {
+        SVGOutputFormat format = new SVGOutputFormat();
+        format.setPrettyPrint(true);
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+
+        format.write(buf, p.getDrawing());
+        String source = buf.toString("UTF-8");
+
+        return source;
+    }
+
+    private JDialog createNewDialog(SVGView p, JTextArea ta) {
+        JDialog dialog = new JDialog((Frame) SwingUtilities.getWindowAncestor(p.getComponent()));
+        p.putClientProperty(DIALOG_CLIENT_PROPERTY, dialog);
+        dialog.setTitle(p.getTitle());
+        dialog.setResizable(true);
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        ta.setWrapStyleWord(true);
+        ta.setLineWrap(true);
+        JScrollPane sp = new JScrollPane(ta);
+        //sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        dialog.getContentPane().add(sp);
+        dialog.setSize(400, 400);
+        dialog.setLocationByPlatform(true);
+
+        return dialog;
+    }
+
+    private JDialog getExistingDialog(SVGView p) {
+        return (JDialog) p.getClientProperty(DIALOG_CLIENT_PROPERTY);
+    }
+
+    public JDialog getDialog() {
+        return dialog;
+    }
+
+    public JTextArea getTa() {
+        return ta;
+    }
+
+    public String getSource() {
+        return source;
+    }
+}
