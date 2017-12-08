fb7522ac234133bfb4085875eaba7bd9fbfa9958
Karol Zdunek <karolzdunek3@gmail.com>
2017-12-08 11:58:46 +0100

    Merge origin/master into Comics
    Conflicts:
    	JHotDraw/src/main/java/org/jhotdraw/samples/svg/SVGDrawingPanel.java

---
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/gui/ComicsToolBarTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jhotdraw.samples.svg.gui;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Karol Zdunek
+ */
+public class ComicsToolBarTest {
+    
+    public ComicsToolBarTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+//        ComicsToolBar frame = GuiActionRunner.execute(() -> new ComicsToolBarTest());
+  //      window = new FrameFixture(frame);
+    //    window.show(); // shows the frame to test
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
