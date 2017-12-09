bbc02e9bf3cae93b17e130b3d428baba41b43034
karim møller <karimkuku@gmail.com>
2017-12-09 22:59:02 +0100

    Done with stroke

--- JHotDraw/src/main/java/org/jhotdraw/draw/FigurePainter.java
+++ JHotDraw/src/main/java/org/jhotdraw/draw/FigurePainter.java
@@ -5,23 +5,29 @@
  */
 package org.jhotdraw.draw;
 
+import java.awt.Color;
 import java.awt.event.MouseEvent;
+import static org.jhotdraw.draw.AttributeKeys.STROKE_COLOR;
+import static org.jhotdraw.draw.AttributeKeys.STROKE_COLOR_LEFT_MOUSE;
+import static org.jhotdraw.draw.AttributeKeys.STROKE_COLOR_RIGHT_MOUSE;
 
 /**
  *
  * @author karim
  */
 public class FigurePainter {
-    
-    public Figure paint(Figure figure, MouseEvent event, DrawingEditor editor){
-        
-       switch(event.getButton()){
-           case 1:
-               System.out.println("im pressed");
-           case 2:
-               System.out.println("im pressed");
-       }
-          return null;
+
+    public void paint(Figure figure, MouseEvent event, DrawingEditor editor) {
+
+        switch (event.getButton()) {
+            case 1:
+                // LEFT MOUSE
+                figure.setAttribute(STROKE_COLOR, figure.getAttribute(STROKE_COLOR_LEFT_MOUSE));
+                break;
+            case 3:
+                // RIGHT MOUSE
+                figure.setAttribute(STROKE_COLOR, figure.getAttribute(STROKE_COLOR_RIGHT_MOUSE));
+        }
     }
-    
+
 }
