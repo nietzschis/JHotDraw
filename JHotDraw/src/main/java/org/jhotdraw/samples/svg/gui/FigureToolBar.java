7471581910a8148c8728ab7f01daa7628bb649bd
vapha15 <vapha15@student.sdu.dk>
2017-12-11 19:36:21 +0100

    add new contrast Slider in figure Toolbar

--- JHotDraw/src/main/java/org/jhotdraw/samples/svg/gui/FigureToolBar.java
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/gui/FigureToolBar.java
@@ -21,6 +21,7 @@ import org.jhotdraw.util.*;
 
 import java.awt.*;
 import javax.swing.*;
+import static javax.swing.SwingConstants.SOUTH_EAST;
 import javax.swing.plaf.LabelUI;
 import javax.swing.plaf.SliderUI;
 import javax.swing.text.DefaultFormatter;
@@ -102,6 +103,33 @@ public class FigureToolBar extends AbstractToolBar {
                     opacitySlider.setUI((SliderUI) PaletteSliderUI.createUI(opacitySlider));
                     opacitySlider.setScaleFactor(100d);
                     new FigureAttributeEditorHandler<Double>(OPACITY, opacitySlider, editor);
+                    
+                    
+                        // Contrast slider
+                    JPopupButton opacityPopupButton2 = new JPopupButton();
+                  
+                    JAttributeSlider contrastSlider = new JAttributeSlider(JSlider.VERTICAL, 0, 100, 100);
+                    opacityPopupButton2.add(contrastSlider);
+                    labels.configureToolBarButton(opacityPopupButton2, "attribute.figureContrast");
+                    opacityPopupButton2.setUI((PaletteButtonUI) PaletteButtonUI.createUI(opacityPopupButton2));
+                    opacityPopupButton2.setIcon(
+                            new SelectionOpacityIcon(editor, CONTRAST, FILL_COLOR, STROKE_COLOR, getClass().getResource(labels.getString("attribute.figureContrast.icon")),
+                            new Rectangle(5, 5, 6, 6), new Rectangle(4, 4, 7, 7)));
+                    opacityPopupButton2.setPopupAnchor(SOUTH_EAST);
+                  
+                 
+                   // new SelectionComponentRepainter(editor, opacityPopupButton2);
+                    gbc = new GridBagConstraints();
+                    gbc.gridx = 3;
+                    gbc.gridy = 0;
+                    gbc.insets = new Insets(0, 0, 0, 0);
+                    
+                    gbc.weighty=1;
+                    gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+                    p.add(opacityPopupButton2, gbc);
+                    contrastSlider.setUI((SliderUI) PaletteSliderUI.createUI(contrastSlider));
+                    contrastSlider.setScaleFactor(100d);
+                    new FigureAttributeEditorHandler<Double>(CONTRAST, contrastSlider, editor);
                 }
                 break;
 
