88ea606d19d89171bb93e9c0201e5adec5bd57ea
Daniel Holst <danho15@student.sdu.du>
2017-12-11 16:40:44 +0100

    Refactored AbstractToolbar | Implemeted change an RecordingToolbar

--- JHotDraw/src/main/java/org/jhotdraw/samples/svg/gui/AbstractToolBar.java
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/gui/AbstractToolBar.java
@@ -16,11 +16,16 @@ package org.jhotdraw.samples.svg.gui;
 import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
 import java.awt.*;
 import java.beans.*;
+import java.util.ArrayList;
 import java.util.prefs.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.ButtonUI;
 import org.jhotdraw.app.JHotDrawFeatures;
 import org.jhotdraw.gui.*;
 import org.jhotdraw.draw.*;
+import org.jhotdraw.gui.plaf.palette.PaletteButtonUI;
+import org.jhotdraw.util.ResourceBundleUtil;
 
 /**
  * AbstractToolBar.
@@ -35,6 +40,8 @@ public /*abstract*/ class AbstractToolBar extends JDisclosureToolBar {
     private JComponent[] panels;
     protected Preferences prefs;
     protected PropertyChangeListener eventHandler;
+    private ArrayList<JButton> toolButtons = new ArrayList<JButton>();
+    ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.samples.svg.Labels");
 
     /** Creates new form. */
     @FeatureEntryPoint(JHotDrawFeatures.TOOL_PALETTE)
@@ -82,6 +89,17 @@ public /*abstract*/ class AbstractToolBar extends JDisclosureToolBar {
         }
         return eventHandler;
     }
+    
+    
+    
+    protected JButton AddToolButton(Action action, String name, String labelsKey){
+        JButton btn = new JButton(action);
+        btn.setName(name);
+        btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
+        labels.configureToolBarButton(btn, labelsKey);
+        toolButtons.add(btn);
+        return btn;
+    }
 
     public void setEditor(DrawingEditor editor) {
         if (this.editor != null) {
@@ -111,8 +129,19 @@ public /*abstract*/ class AbstractToolBar extends JDisclosureToolBar {
         return panels[state];
     }
 
-    /*abstract*/ protected JComponent createDisclosedComponent(int state) {
-        return null;
+
+    
+    protected JComponent createDisclosedComponent(int state) {
+        GridLayout layout = new GridLayoutSquare(3,
+                toolButtons.size() / 3);
+        layout.setHgap(4);
+        layout.setVgap(4);
+        JPanel panel = new JPanel(layout);
+        panel.setBorder(new EmptyBorder(5, 5, 5, 8));
+        toolButtons.forEach((btn) -> {
+            panel.add(btn);
+        });
+        return panel;
     }
 
     protected int getDefaultDisclosureState() {
--- JHotDraw/src/main/java/org/jhotdraw/samples/svg/gui/RecordingToolBar.java
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/gui/RecordingToolBar.java
@@ -1,78 +1,38 @@
 package org.jhotdraw.samples.svg.gui;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
-import java.util.prefs.Preferences;
 import javax.swing.AbstractButton;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import org.jhotdraw.draw.DrawingEditor;
-import org.jhotdraw.gui.plaf.palette.PaletteButtonUI;
 import org.jhotdraw.recording.RecordingManager;
 import org.jhotdraw.util.ResourceBundleUtil;
 
 public class RecordingToolBar extends AbstractToolBar {    
-    public RecordingToolBar() {
-        ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.samples.svg.Labels");
-        setName(labels.getString("recording.toolbar"));
-    }
 
     RecordingManager recordingManager = null;
     ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.samples.svg.Labels");
 
     protected AbstractButton recordButton = null;
     protected ArrayList<Integer> listOfActiveRecordings = new ArrayList<>();
-
+    
+    public RecordingToolBar() {
+        ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.samples.svg.Labels");
+        setName(labels.getString("recording.toolbar"));
+    }
+        
     @Override
-    protected JComponent createDisclosedComponent(int state) {
-        JPanel p = null;
-
-        switch (state) {
-            case 1: {
-                p = new JPanel();
-                p.setOpaque(false);
-                p.setBorder(new EmptyBorder(5, 5, 5, 8));
-
-                Preferences prefs = Preferences.userNodeForPackage(getClass());
-
-                GridBagLayout layout = new GridBagLayout();
-                p.setLayout(layout);
-
-                GridBagConstraints gbc;
-                AbstractButton btn;
-
-                
-                // Start Recording Btn
-                btn = new JButton(recordingManager.getStartRecordingAction());
-                btn.setName("btnStartRecording");
-                recordButton = btn;
-                btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
-                labels.configureToolBarButton(btn, "recordingTool.start");
-
-                btn.addActionListener((ActionEvent e) -> {
-                    listOfActiveRecordings.add(editor.getActiveView().getDrawing().hashCode());
-                    updateRecordButton();
-                });
-
-                gbc = new GridBagConstraints();
-                gbc.gridy = 0;
-                gbc.gridx = 0;
-                gbc.insets = new Insets(0, 0, 0, 0);
-                p.add(btn, gbc);
-
-                // Stop Recording Btn
-                btn = new JButton(recordingManager.getStopRecordingAction());
-                btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
-                labels.configureToolBarButton(btn, "recordingTool.stop");
-
-                btn.addActionListener((ActionEvent e) -> {
+    public void setEditor(DrawingEditor editor) {
+        super.setEditor(editor);
+        recordingManager = new RecordingManager(editor);
+        
+        recordButton = AddToolButton(recordingManager.getStartRecordingAction(), "btnStartRecording", "recordingTool.start");
+        recordButton.addActionListener((ActionEvent e) -> {
+            listOfActiveRecordings.add(editor.getActiveView().getDrawing().hashCode());
+            updateRecordButton();
+        });
+        
+        AddToolButton(recordingManager.getStopRecordingAction(), "btnStopRecording", "recordingTool.stop").
+                addActionListener((ActionEvent e) -> {
                     if (listOfActiveRecordings.contains(editor.getActiveView().getDrawing().hashCode())) {
                         listOfActiveRecordings.remove(
                                 listOfActiveRecordings.indexOf(editor.getActiveView().getDrawing().hashCode())
@@ -81,36 +41,7 @@ public class RecordingToolBar extends AbstractToolBar {
                     updateRecordButton();
                 });
 
-                gbc = new GridBagConstraints();
-                gbc.gridy = 1;
-                gbc.gridx = 0;
-                gbc.insets = new Insets(5, 0, 0, 0);
-                p.add(btn, gbc);
-                
-                // Play Recording Btn
-                btn = new JButton(recordingManager.getPlayFramesAction());
-                btn.setName("btnPlayRecording");
-                btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
-                labels.configureToolBarButton(btn, "recordingTool.play");
-                gbc = new GridBagConstraints();
-                gbc.gridy = 2;
-                gbc.gridx = 0;
-                gbc.insets = new Insets(5, 0, 0, 0);
-                p.add(btn, gbc);
-            }
-            break;
-        }
-
-        editor.getActiveView().addPropertyChangeListener(new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-                if (evt.getPropertyName() == "drawing") {
-                    updateRecordButton();
-                }
-            }
-        });
-
-        return p;
+        AddToolButton(recordingManager.getPlayFramesAction(), "btnPlayRecording", "recordingTool.play");
     }
 
     protected void updateRecordButton() {
@@ -131,9 +62,5 @@ public class RecordingToolBar extends AbstractToolBar {
         return 1;
     }
 
-    @Override
-    public void setEditor(DrawingEditor editor) {
-        super.setEditor(editor);
-        recordingManager = new RecordingManager(editor);
-    }
+    
 }
