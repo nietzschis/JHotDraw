b9530bc85bb107305bd1b6e3414714a049719fc8
Marc Kristensen <marcndkk@gmail.com>
2017-12-11 18:50:48 +0100

    Implemented unit tests for SVGInputFormat

--- JHotDraw/src/test/java/org/jhotdraw/samples/svg/io/SVGInputFormatTest.java
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/io/SVGInputFormatTest.java
@@ -5,6 +5,13 @@
  */
 package org.jhotdraw.samples.svg.io;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import org.jhotdraw.draw.Figure;
+import org.jhotdraw.draw.QuadTreeDrawing;
+import org.jhotdraw.samples.svg.figures.SVGRectFigure;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,28 +25,54 @@ import static org.junit.Assert.*;
  */
 public class SVGInputFormatTest {
     
+    private QuadTreeDrawing drawing;
+    private SVGInputFormat inputFormat;
+    private InputStream in;
+    
     public SVGInputFormatTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
     }
     
     @Before
     public void setUp() {
+        drawing = new QuadTreeDrawing();
+        inputFormat = new SVGInputFormat();
     }
     
     @After
     public void tearDown() {
+        drawing = null;
+        inputFormat = null;
+        in = null;
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+    @Test
+    public void testRectangle() throws IOException{
+        String xml =    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                        "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.2\" baseProfile=\"tiny\">\n" +
+                        "    <defs/>\n" +
+                        "    <rect x=\"708\" y=\"408\" width=\"303\" height=\"230\" fill=\"#fff\" stroke=\"#000\"/>\n" +
+                        "</svg>";
+        in = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
+        inputFormat.read(in, drawing, true);
+        
+        assertTrue(drawing.getChildCount() == 1);
+        Figure figure = drawing.getChild(0);
+        assertTrue(figure instanceof SVGRectFigure);
+        SVGRectFigure rect = (SVGRectFigure) figure;
+        
+        assertTrue(rect.getX() == 708);
+        assertTrue(rect.getY() == 408);
+        assertTrue(rect.getWidth() == 303);
+        assertTrue(rect.getHeight() == 230);
+    }
+    
+    @Test(expected = IOException.class)
+    public void testInvalidXML() throws IOException{
+        String xml = "Definitely not XML";
+        in = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
+        inputFormat.read(in, drawing, true);
+    }
+    
+    
 }
