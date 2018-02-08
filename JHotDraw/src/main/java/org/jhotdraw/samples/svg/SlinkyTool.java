74a6fae72f01234ce4ca0e53de998a785d1f8c63
Jens <jlaur16@student.sdu.dk>
2018-02-08 23:08:10 +0100

    Slinky third commit

--- JHotDraw/src/main/java/org/jhotdraw/samples/svg/SlinkyTool.java
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/SlinkyTool.java
@@ -5,6 +5,8 @@ import org.jhotdraw.draw.AttributeKey;
 import org.jhotdraw.samples.svg.figures.SVGBezierFigure;
 import org.jhotdraw.samples.svg.figures.SVGPathFigure;
 import org.jhotdraw.samples.svg.figures.SVGEllipseFigure;
+import org.jhotdraw.samples.svg.figures.SVGRectFigure;
+import org.jhotdraw.samples.svg.figures.SVGTriangleFigure;
 
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
@@ -40,19 +42,18 @@ public class SlinkyTool extends PathTool {
     @Override
     public void mouseDragged(MouseEvent evt) {
         
-        
+        int figureSize = 35; 
+        //Paints figure 
         FigurePainter painter = new FigurePainter();
         painter.paint(evt.getButton(), editor);
         getView().clearSelection();
         createdFigure = createSpray();
         Point2D.Double p = constrainPoint(viewToDrawing(anchor));
-        anchor.x = evt.getX()-25;
-        anchor.y = evt.getY()-25;
+        anchor.x = evt.getX()-figureSize;
+        anchor.y = evt.getY()-figureSize;
         createdFigure.setBounds(p, p);
         getDrawing().add(createdFigure);
-        createdFigure.setBounds(constrainPoint(new Point(anchor.x+50, anchor.y+50)),p);
-
-    
+        createdFigure.setBounds(constrainPoint(new Point(anchor.x+(figureSize*2), anchor.y+(figureSize*2))),p);
     }
     
 
@@ -60,11 +61,13 @@ public class SlinkyTool extends PathTool {
     protected Figure createSpray() {
         Figure f = (Figure) prototype.clone();
         getEditor().applyDefaultAttributesTo(f);
+        /*
         if (prototypeAttributes != null) {
             for (Map.Entry<AttributeKey, Object> entry : prototypeAttributes.entrySet()) {
                 entry.getKey().basicSet(f, entry.getValue());
             }
         }
+        */
         return f;
     }
 }
